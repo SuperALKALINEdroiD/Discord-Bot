5dafe296684912312e7f251fb929768874a88e9e
AlkalineD <64575780+SuperALKALINEdroiD@users.noreply.github.com>
2020-05-24 22:30:34 +0530

    Adding files

---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Hello') {
+    msg.reply('Hello Buddy!!');
+  }
+else if(msg.content==='Invite'){
+msg.reply('https://discord.com/api/oauth2/authorize?client_id=707561126011076733&permissions=67584&scope=bot');
+}
+});
