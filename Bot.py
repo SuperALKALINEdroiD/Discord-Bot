9817fbc3122e9f69758da87f51354d07c3f875ec
AlkalineD <64575780+SuperALKALINEdroiD@users.noreply.github.com>
2020-08-01 22:46:39 +0530

    update2

--- Bot.py
+++ Bot.py
@@ -5,4 +5,4 @@ client = discord.Client()
 async def on_message(message):
     await message.channel.send("Hi")
 
-client.run("NzA3NTYxMTI2MDExMDc2NzMz.XrKmGQ.G6A1lTqs0zLdYAEvatXJ562nPdU")
+client.run("Token Here")
