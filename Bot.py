95de35ad216b5741f7a1e186a4b0f4e5881418e4
AlkalineD <64575780+SuperALKALINEdroiD@users.noreply.github.com>
2020-08-03 21:22:57 +0530

    reply with same message

--- Bot.py
+++ Bot.py
@@ -3,6 +3,9 @@ import discord
 client = discord.Client()
 @client.event
 async def on_message(message):
-    await message.channel.send(message.content)
+    if message.author.bot:
+        pass
+    else:
+        await message.channel.send(message.content)
 
-client.run("Token")
+client.run("NzM5MTcxODA1NzI5OTE1MDMw.XyWlYw.dmm0m_ZJY-jwK3-BoWVZRWXHjqU")
