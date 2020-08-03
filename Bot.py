0b612e70371b5de726d168ac36a83b4a554360c3
AlkalineD <64575780+SuperALKALINEdroiD@users.noreply.github.com>
2020-08-03 21:32:07 +0530

    token...err

--- Bot.py
+++ Bot.py
@@ -8,4 +8,4 @@ async def on_message(message):
     else:
         await message.channel.send(message.content)
 
-client.run("NzM5MTcxODA1NzI5OTE1MDMw.XyWlYw.dmm0m_ZJY-jwK3-BoWVZRWXHjqU")
+client.run("Token")
