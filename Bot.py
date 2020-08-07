e2b2f9ee23184fbd030c09dfe53b21a250149250
AlkalineD <64575780+SuperALKALINEdroiD@users.noreply.github.com>
2020-08-07 22:50:01 +0530

    enhanced adding

--- Bot.py
+++ Bot.py
@@ -4,18 +4,22 @@ import string
 client = discord.Client()
 @client.event
 async def on_message(message):
+    add_result = 0
     if message.author.bot:
         pass
     elif  message.content.startswith("add"):
         user_msg = message.content
         message_content = user_msg.split()
-        try:
-            number_1 = (int)(message_content[1])
-            number_2 = (int)(message_content[2])
-            await message.channel.send(number_1 + number_2)
-        except:
-            await message.channel.send("Error needed 2 numbers")
+        if len(message_content) == 2:
+            await message.channel.send("addition requires more than 2 operands")
+        else:
+            for i in range(1, len(message_content)):
+                add_result = add_result + int(message_content[i])
+
+            await message.channel.send(add_result)
 
 
 
-client.run("TOken")
+
+
+client.run("Token")
