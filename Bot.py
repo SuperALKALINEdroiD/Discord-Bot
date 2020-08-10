7fa6b808bb5682df685233c18fe4c8b216e59639
AlkalineD <64575780+SuperALKALINEdroiD@users.noreply.github.com>
2020-08-10 22:03:46 +0530

    +-*/

--- Bot.py
+++ Bot.py
@@ -2,16 +2,48 @@ import discord
 client = discord.Client()
 @client.event
 async def on_message(message):
-    add_result = 0
+    result = 0                                                                           # operation result
+    md_result = 1
     if message.author.bot:
         pass
-    elif  message.content.startswith("add"):
+    elif message.content.startswith("add"):
         user_msg = message.content
         message_content = user_msg.split()
-        if len(message_content) == 2:
+        if len(message_content) == 2:                                                       # case of 1 operand
             await message.channel.send("addition requires more than one operands")
         else:
             for i in range(1, len(message_content)):
-                add_result = add_result + int(message_content[i])
-            await message.channel.send(add_result)
-client.run("Token")
+                result = result + int(message_content[i])
+            await message.channel.send(result)
+    elif message.content.startswith("sub"):
+        user_msg = message.content
+        message_content = user_msg.split()
+        if len(message_content) == 2:                                                        # case of 1 operand
+            await message.channel.send("Subtraction requires more than one operands")
+        else:
+            result = int(message_content[1])
+            for i in range(2, len(message_content)):
+                result = result - int(message_content[i])
+            await message.channel.send(result)
+    elif message.content.startswith("multiply"):
+        user_msg = message.content
+        message_content = user_msg.split()
+        if len(message_content) == 2:                                                       # case of 1 operand
+            await message.channel.send("Multiplication requires more than one operands")
+        else:
+            for i in range(1, len(message_content)):
+                md_result = md_result * int(message_content[i])
+            await message.channel.send(md_result)
+    elif message.content.startswith("div"):
+        user_msg = message.content
+        message_content = user_msg.split()
+        if len(message_content) == 2:                                                       # case of 1 operand
+            await message.channel.send("Division requires more than one operands")
+        else:
+            md_result = int(message_content[1])
+            for i in range(2, len(message_content)):
+                md_result = md_result / int(message_content[i])
+            await message.channel.send(md_result)
+
+
+client.run("NzM5MTcxODA1NzI5OTE1MDMw.XyWlYw.e_ouGxklov96CfcDQ9eRvMWXPFE")
