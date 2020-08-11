959d90242f7f091f7d7c2d4e49cd3398d59ca391
AlkalineD <64575780+SuperALKALINEdroiD@users.noreply.github.com>
2020-08-11 22:24:53 +0530

    remainder calculation

--- Bot.py
+++ Bot.py
@@ -44,6 +44,17 @@ async def on_message(message):
             for i in range(2, len(message_content)):
                 md_result = md_result / int(message_content[i])
             await message.channel.send(md_result)
+    elif message.content.startswith("rem"):
+        user_msg = message.content
+        message_content = user_msg.split()
+        if len(message_content) > 3:
+            await message.channel.send("Not Supported yet, Unsure what would it mean to do")
+        else:
+            number1 = int(message_content[1])
+            number2 = int(message_content[2])
+            md_result = number1 % number2
+            await message.channel.send(md_result)
+
 
 
 client.run("Token")
