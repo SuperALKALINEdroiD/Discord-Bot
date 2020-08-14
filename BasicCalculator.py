a016150fdd9a19521e3dc1622a6ae53202e185df
AlkalineD <64575780+SuperALKALINEdroiD@users.noreply.github.com>
2020-08-14 23:25:58 +0530

    Basic Calculator complete but still needs some improvements

--- BasicCalculator.py
+++ BasicCalculator.py
@@ -24,3 +24,43 @@ class subtract:
             for i in range(2, len(message)):
                 sub_result -= int(message[i])
             return sub_result
+
+class multiply:
+    def __init__(self, messageContent):
+        self.messageContent = messageContent
+
+    def get_result(self):
+        message = self.messageContent.split()
+        if len(message) == 2:
+            return message[1]
+        else:
+            multiply_result = 1
+            for i in range(1, len(message)):
+                multiply_result *= int(message[i])
+            return multiply_result
+
+class division:
+    def __init__(self, messageContent):
+        self.messageContent = messageContent
+
+    def get_result(self):
+        message = self.messageContent.split()
+        if len(message) == 2:
+            return message[1]
+        else:
+            div_result = int(message[1])
+            for i in range(2, len(message)):
+                div_result /= int(message[i])
+            return div_result
+
+class remainder:
+    def __init__(self, messageContent):
+        self.messageContent = messageContent
+
+    def get_result(self):
+        message = self.messageContent.split()
+        if len(message) > 3:
+            return "Error"
+        else:
+            remResult = int(message[1]) % int(message[2])
+            return remResult
--- Bot.py
+++ Bot.py
@@ -1,5 +1,5 @@
 import discord
-from BasicCalculator import add, subtract
+from BasicCalculator import add, subtract, multiply, division, remainder
 client = discord.Client()
 @client.event
 async def on_message(message):
@@ -8,42 +8,18 @@ async def on_message(message):
         pass
     elif message.content.startswith("add"):
         addResult = add(message.content)
-        plus = addResult.getResult()
-        await message.channel.send(plus)
+        await message.channel.send(addResult.getResult())
     elif message.content.startswith("sub"):
         subResult = subtract(message.content)
-        minus = subResult.get_result()
-        await message.channel.send(minus)
+        await message.channel.send(subResult.get_result())
     elif message.content.startswith("multiply"):
-        user_msg = message.content
-        message_content = user_msg.split()
-        if len(message_content) == 2:                                                       # case of 1 operand
-            await message.channel.send("Multiplication requires more than one operands")
-        else:
-            for i in range(1, len(message_content)):
-                md_result = md_result * int(message_content[i])
-            await message.channel.send(md_result)
+        mul_result = multiply(message.content)
+        await message.channel.send( mul_result.get_result())
     elif message.content.startswith("div"):
-        user_msg = message.content
-        message_content = user_msg.split()
-        if len(message_content) == 2:                                                       # case of 1 operand
-            await message.channel.send("Division requires more than one operands")
-        else:
-            md_result = int(message_content[1])
-            for i in range(2, len(message_content)):
-                md_result = md_result / int(message_content[i])
-            await message.channel.send(md_result)
+        divReult = division(message.content)
+        await message.channel.send(divReult.get_result())
     elif message.content.startswith("rem"):
-        user_msg = message.content
-        message_content = user_msg.split()
-        if len(message_content) > 3:
-            await message.channel.send("Not Supported yet, Unsure what would it mean to do")
-        else:
-            number1 = int(message_content[1])
-            number2 = int(message_content[2])
-            md_result = number1 % number2
-            await message.channel.send(md_result)
-
-
+        remResult = remainder(message.content)
+        await message.channel.send(remResult.get_result())
 
 client.run("Token")
